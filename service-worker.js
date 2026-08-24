d4ecb9426e2663aee543155667c3c0d2967a844d
Marileons <marileo64@libero.it>
2026-08-24 11:46:24 +0200

    Agenda Istruttori v1-21 r10 report allievo e documenti finali

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="agenda-istruttori-v1-21-premium-r10-title-typography";
+const CACHE="agenda-istruttori-v1-21-premium-r10-document-button-accents";
 const FILES=[
   "./",
   "./index.html",
